ff421ce9e94bd6d0428ac18275665e5fe7ba0e3d
Julia Heller <julia.natsuko@gmail.com>
2020-12-19 15:26:26 +0100

    Get categories with fetch.

--- src/components/categories/CategoryList.jsx
+++ src/components/categories/CategoryList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -7,8 +7,7 @@ import Avatar from "@material-ui/core/Avatar";
 import ListItemText from "@material-ui/core/ListItemText";
 import Paper from "@material-ui/core/Paper";
 import Divider from "@material-ui/core/Divider";
-
-import { categories, posts } from "../../mocks/categories";
+import categoryService from '../../services/category.service';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -18,8 +17,20 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function CategoryList(categories) {
+export default function CategoryList() {
     const classes = useStyles();
+    const [categories, setCategories] = useState([]);
+
+    useEffect( () => {
+      
+        const fetchCategories = async () => {
+            const getCategories = await categoryService.getAll();
+         
+            setCategories(getCategories);
+            console.log(categories);
+        }
+      fetchCategories();
+    }, [categories]);
 
     return (
         <Paper variant="outlined" className={classes.root}>
