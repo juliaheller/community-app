540f2234266bfe6875f13b748c7948e55d46a249
Julia Heller <julia.natsuko@gmail.com>
2020-12-23 16:14:57 +0100

    Get authenticated user after login.

--- src/redux/auth/auth.actions.js
+++ src/redux/auth/auth.actions.js
@@ -4,12 +4,15 @@ import authService from "../../services/auth.service";
 export const login = (email, password) => async (dispatch) => {
 	try {
 		const { token, error } = await authService.loginUser(email, password);
-		if (token) {
+		const me = await authService.me();
+		console.log(me);
+		if (token && me) {
 			dispatch({
 				type: LOGIN,
-				payload: { isLoggedIn: true, me: {} }
+				payload: { isLoggedIn: true, me: me },
 			});
 			localStorage.setItem("token", token);
+
 			return Promise.resolve();
 		} else {
 			return Promise.reject(error);
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,22 +1,33 @@
-// import auth from "../helper/auth";
+import auth from "../helper/auth";
 require("dotenv").config();
 
 const apiURL = process.env.REACT_APP_API_URL + "/auth";
 
 export default {
-    async loginUser(email, password) {
-        const requestOptions = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                email: email,
-                password: password,
-            }),
-        };
-        console.log(apiURL);
-        const response = await fetch(apiURL + "/login", requestOptions);
-        return await response.json();
-    },
+	async loginUser(email, password) {
+		const requestOptions = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				email: email,
+				password: password,
+			}),
+		};
+		const response = await fetch(apiURL + "/login", requestOptions);
+		return await response.json();
+	},
+
+	async me() {
+		const requestOptions = {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				...auth.getAuthHeader(),
+			},
+		};
+		const response = await fetch(apiURL + "/me", requestOptions);
+		return await response.json();
+	},
 };
