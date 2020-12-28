ee6171b00dc5bbd72f29d688e56d4dc47ef1d46d
Julia Heller <julia.natsuko@gmail.com>
2020-12-28 10:51:28 +0100

    Add comments service.

---
+++ src/services/comments.service.js
@@ -0,0 +1,53 @@
+import auth from "../helper/auth";
+require("dotenv").config();
+
+const apiURL = process.env.REACT_APP_API_URL + "/categories";
+
+export default {
+	async getComments(postId, categoryId) {
+		const requestOptions = {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				...auth.getAuthHeader(),
+			},
+		};
+		const response = await fetch(
+			`${apiURL}/${categoryId}/posts/${postId}/comments/`,
+			requestOptions
+		);
+		return await response.json();
+	},
+
+	async createComment(comment, postId, categoryId) {
+		const requestOptions = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				...auth.getAuthHeader(),
+			},
+			body: JSON.stringify(comment),
+		};
+		const response = await fetch(
+			`${apiURL}/${categoryId}/posts/${postId}/comments/`,
+			requestOptions
+		);
+		return await response.json();
+	},
+
+	async deleteComment(id, categoryId, postId) {
+		const requestOptions = {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json",
+				...auth.getAuthHeader(),
+			},
+		};
+		const response = await fetch(
+			`${apiURL}/${categoryId}/posts/${postId}/comments/${id}`,
+			requestOptions
+		);
+
+		return await response.json();
+	},
+};
