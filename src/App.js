ae8124a3cae65af46ef0e917a13584e0a4c5bd0d
Julia Heller <julia.heller@spreewunder.com>
2021-11-13 13:28:15 +0100

    Use createTheme

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "fontsource-roboto";
 import "./App.css";
 import { ThemeProvider } from "@material-ui/core/styles";
-import { createMuiTheme } from "@material-ui/core/styles";
+import { createTheme } from "@material-ui/core/styles";
 
 // Components
 import AppBar from "../src/components/menu/AppBar";
@@ -29,86 +29,82 @@ import { loginFromLocalStorage } from "../src/redux/auth/auth.actions";
 import store from "../src/redux/store";
 import Event from "./pages/events/Event";
 
-const theme = createMuiTheme({
-    palette: {
-        primary: {
-            // light: will be calculated from palette.primary.main,
-            main: "#1C304A",
-            // dark: will be calculated from palette.primary.main,
-            // contrastText: will be calculated to contrast with palette.primary.main
-        },
-        secondary: {
-            light: "#613291",
-            main: "#660099",
-            // dark: will be calculated from palette.secondary.main,
-            contrastText: "#FFFFFF",
-        },
-        // Used by `getContrastText()` to maximize the contrast between
-        // the background and the text.
-        contrastThreshold: 3,
-        // Used by the functions below to shift a color's luminance by approximately
-        // two indexes within its tonal palette.
-        // E.g., shift from Red 500 to Red 300 or Red 700.
-        tonalOffset: 0.2,
-    },
+const theme = createTheme({
+	palette: {
+		primary: {
+			// light: will be calculated from palette.primary.main,
+			main: "#1C304A",
+			// dark: will be calculated from palette.primary.main,
+			// contrastText: will be calculated to contrast with palette.primary.main
+		},
+		secondary: {
+			light: "#613291",
+			main: "#660099",
+			// dark: will be calculated from palette.secondary.main,
+			contrastText: "#FFFFFF",
+		},
+		// Used by `getContrastText()` to maximize the contrast between
+		// the background and the text.
+		contrastThreshold: 3,
+		// Used by the functions below to shift a color's luminance by approximately
+		// two indexes within its tonal palette.
+		// E.g., shift from Red 500 to Red 300 or Red 700.
+		tonalOffset: 0.2,
+	},
 });
 
 function App() {
-    store.dispatch(loginFromLocalStorage());
-    return (
-        <div className="App">
-            <ThemeProvider theme={theme}>
-                <Router>
-                    <AppBar></AppBar>
-                    <Switch>
-                        <Route path="/login" component={Login}></Route>
-                        <ProtectedRoute path="/witches" component={Witches} />
-                        <ProtectedRoute
-                            exact
-                            path="/"
-                            component={Home}></ProtectedRoute>
-                        <ProtectedRoute
-                            path="/events"
-                            component={Events}></ProtectedRoute>
-                        <ProtectedRoute
-                            path="/messages"
-                            component={Messages}></ProtectedRoute>
-                        <ProtectedRoute
-                            path="/photos"
-                            component={Photos}></ProtectedRoute>
-                        <ProtectedRoute
-                            path="/bookofshadows"
-                            component={BookOfShadows}></ProtectedRoute>
-                        <ProtectedRoute
-                            path="/forum"
-                            component={Forum}></ProtectedRoute>
-                        <ProtectedRoute
-                            path="/profile"
-                            component={Profile}></ProtectedRoute>
-                        <ProtectedRoute
-                            path="/categories/:categoryId/newpost"
-                            component={PostForm}></ProtectedRoute>
-                        <ProtectedRoute
-                            path="/categories/:categoryId/post/:id"
-                            component={Post}></ProtectedRoute>
-                        <ProtectedRoute
-                            path="/categories/:id"
-                            component={Category}></ProtectedRoute>
-                        <ProtectedRoute
-                            path="/events/event/:id"
-                            component={Event}></ProtectedRoute>
-                        <ProtectedRoute
-                            path="/newevent"
-                            component={EventForm}></ProtectedRoute>
-                        <Route
-                            path="*"
-                            component={() => "404 Page not found"}
-                        />
-                    </Switch>
-                </Router>
-            </ThemeProvider>
-        </div>
-    );
+	store.dispatch(loginFromLocalStorage());
+	return (
+		<div className='App'>
+			<ThemeProvider theme={theme}>
+				<Router>
+					<AppBar></AppBar>
+					<Switch>
+						<Route path='/login' component={Login}></Route>
+						<ProtectedRoute path='/witches' component={Witches} />
+						<ProtectedRoute exact path='/' component={Home}></ProtectedRoute>
+						<ProtectedRoute path='/events' component={Events}></ProtectedRoute>
+						<ProtectedRoute
+							path='/messages'
+							component={Messages}
+						></ProtectedRoute>
+						<ProtectedRoute path='/photos' component={Photos}></ProtectedRoute>
+						<ProtectedRoute
+							path='/bookofshadows'
+							component={BookOfShadows}
+						></ProtectedRoute>
+						<ProtectedRoute path='/forum' component={Forum}></ProtectedRoute>
+						<ProtectedRoute
+							path='/profile'
+							component={Profile}
+						></ProtectedRoute>
+						<ProtectedRoute
+							path='/categories/:categoryId/newpost'
+							component={PostForm}
+						></ProtectedRoute>
+						<ProtectedRoute
+							path='/categories/:categoryId/post/:id'
+							component={Post}
+						></ProtectedRoute>
+						<ProtectedRoute
+							path='/categories/:id'
+							component={Category}
+						></ProtectedRoute>
+						<ProtectedRoute
+							path='/events/event/:id'
+							component={Event}
+						></ProtectedRoute>
+						<ProtectedRoute
+							path='/newevent'
+							component={EventForm}
+						></ProtectedRoute>
+						<Route path='*' component={() => "404 Page not found"} />
+					</Switch>
+				</Router>
+			</ThemeProvider>
+		</div>
+	);
 }
 
 export default App;
